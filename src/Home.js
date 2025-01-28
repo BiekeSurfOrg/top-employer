2853647913a7ab0d5bcce8b45dcb7594d8290f46
BorisStankov98 <boris.stankovv@gmail.com>
2025-01-28 18:27:07 +0200

    removed video

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,6 @@ const Home = () => {
 
     return (
         <div className="full-screen position-relative">
-            <video autoPlay muted loop id="myVideo" className='background-video'>
-                <source src="../assets/background.mp4" type="video/mp4" />
-            </video>
             <div className="full-screen layout">
                 <div className='tile-list'>
                     <Card text='Cyber Fraude Test' route='/questions' />
