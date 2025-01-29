54dafd17b8cc5c7e21f9a3e738a16a4cc8dc7bf5
jf29771 <boris.stankov@kbc.com>
2025-01-29 16:39:54 +0200

    changed the video in the BG and added the top-employer come here one

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="full-screen position-relative">
             <video autoPlay muted loop id="myVideo" className='background-video'>
-                <source src="../assets/Top_Employer_we_won_2160x3840.mp4" type="video/mp4" />
+                <source src="../assets/Top_Employer_comehere2160x3840.mp4" type="video/mp4" />
             </video>
             <div className="full-screen layout">
                 <div className='tile-list'>
