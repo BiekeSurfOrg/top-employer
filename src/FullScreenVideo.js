6a8a731ae6ef1fc2743eed5ad51775d70be2c8d5
jf29771 <boris.stankov@kbc.com>
2025-03-20 08:49:15 +0200

    chaned video

--- src/FullScreenVideo.js
+++ src/FullScreenVideo.js
@@ -16,7 +16,7 @@ const FullScreenVideo = () => {
         <div className='full-screen layout position-relative'>
             <img className='action-icon video-back-button' src='../assets/action-func-chevron-left.webp' onClick={() => routeChange("/")} />
             <video autoPlay loop id="myVideo" className='background-video'>
-                <source src="../assets/diversityday.mp4" type="video/mp4" />
+                <source src="../assets/Top_Employer_we_won_2160x3840.mp4" type="video/mp4" />
             </video>
         </div>
     );
